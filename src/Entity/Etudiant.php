8722e7e3ded879227d2ddb239191813a2b56a582
Moussa CAMARA <msa.camara@gmail.com>
2023-12-07 11:28:00 +0100

    install et demo sf

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtudiantRepository::class)]
+class Etudiant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $matricule = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMatricule(): ?string
+    {
+        return $this->matricule;
+    }
+
+    public function setMatricule(string $matricule): static
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+}
