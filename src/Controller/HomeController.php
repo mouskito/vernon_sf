85db90db90f5e42607c7bcda2a99a5eec2e35646
Moussa CAMARA <msa.camara@gmail.com>
2023-12-07 13:38:30 +0100

    generated login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        //Traitement
+        //Traitement
+        //Fin de traitement
         return $this->render('home/index.html.twig');
     }
 }
